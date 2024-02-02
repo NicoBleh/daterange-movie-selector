8a8c008ac32919d407d19357779af478da48d2a5
Nico Bleh <nicobleh@gmail.com>
2024-02-02 12:41:09 +0100

    create basic express api with neo4j driver

---
+++ api/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var express = require('express');
+
+var app = module.exports = express()
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.get('/movies-in-range', function(req, res){
+  res.send('Movies in range');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
